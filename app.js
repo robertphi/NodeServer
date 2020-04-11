95479c6d7dd20646b5a58baec6bca38c79fc3d3d
robertphi <robertphi@gmail.com>
2020-04-11 12:30:00 -0500

    pushing basic connections for a mongodb and setting up some REST endpoints for units.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var monk = require('monk');
+var db = monk('localhost:27017/mydb');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET units. */
+router.get('/units', function(req, res) {
+  var db = req.db;
+  var collection = db.get('Unit');
+
+  // res.json({test: 'hello'});
+  collection.find({},{},function(e,docs){
+    res.json(docs)
+  });
+});
+
+router.get('/unit/:id', function(req, res) {
+  var db = req.db;
+  var collection = db.get('Unit');
+  var id = req.params.id;
+  var query = { _id : id};
+
+  // res.json({test: 'hello'});
+  collection.find(query,{},function(e,docs){
+    res.json(docs)
+  });
+});
+
+
+
+
 module.exports = router;
