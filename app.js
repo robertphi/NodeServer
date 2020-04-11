d84fa6c675eac16e5b907d90da75b5515f5b6f80
robertphi <robertphi@gmail.com>
2020-04-11 10:48:37 -0500

    Trying to fix some git issues.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//a comment
 var app = express();
 
 app.use(logger('dev'));
