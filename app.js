b4ea7cdaff3fe7e9177e8e84e7fbc050c4e229f0
robertphi <robertphi@gmail.com>
2020-04-11 10:49:12 -0500

    reverting last commit

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-//a comment
 var app = express();
 
 app.use(logger('dev'));
