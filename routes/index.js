c566d6ca2eb21a71c4bc00a0f92cbe3249ff9441
robertphi <robertphi@gmail.com>
2020-04-11 22:02:40 -0500

    Post, update, and delete operations added for unit. Not really handling errors or other crazy things yet, but a good start.

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,38 @@ router.get('/unit/:id', function(req, res) {
   });
 });
 
+router.post('/unit', function (req, res) {
+  var unit = req.body;
+  var db = req.db;
+  var collection = db.get('Unit');
 
+  collection.insert(unit, {}, function(err, data){
+    res.json(data);
+  })
+});
+
+router.put('/unit/:id', function (req, res) {
+  var unit = req.body;
+  var db = req.db;
+  var collection = db.get('Unit');
+  var query = { _id : unit._id};
+  var newValues = { $set: unit};
+
+  collection.update(query, newValues, {}, function(err, data){
+    res.json(data);
+  })
+});
+
+router.delete('/unit/:id', function (req, res) {
+  var db = req.db;
+  var collection = db.get('Unit');
+  var id = req.params.id;
+  var query = { _id : id};
+
+  collection.remove(query, {}, function(err, data){
+    res.json(data);
+  })
+});
 
 
 module.exports = router;
